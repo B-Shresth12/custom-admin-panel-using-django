6bdf0a8bd3927c65a56eea27dff05dbfccfbd5c1
B-Shresth12 <b.shresth12@gmail.com>
2024-07-01 04:09:24 +0545

    admin middleware update

--- admin/middleware.py
+++ admin/middleware.py
@@ -4,38 +4,46 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from admin.models import SiteSetting
-from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.http import HttpResponse, HttpResponseForbidden
 
 
-class AdminMiddleware:
+class AdminMiddleware(View):
+    excluded_urls = [
+        reverse('admin:login'),
+        reverse('admin:logout'),
+        reverse('admin:siteSetting'),
+        reverse('admin:siteSetting.update'),
+    ]
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-
-    @login_required
-    def SiteSettingCheck():
-        # Define excluded URLs where redirection should not occur
-        excluded_urls = [
-            reverse('admin:login'),
-            reverse('admin:logout'),
-            reverse('admin:siteSetting'),
-            reverse('admin:siteSetting.update'),
-        ]
-
-        # Check if the requested URL belongs to the admin app
         if request.path_info.startswith('/admin/'):
-            # Check if the requested URL is excluded
-            if request.path_info in excluded_urls:
+
+            if request.path_info in self.excluded_urls:
                 return self.get_response(request)
-
-            # Check if SiteSetting has the first row
-            try:
-                site_setting = SiteSetting.objects.first()
-            except ObjectDoesNotExist:
-                site_setting = None
-
-            if not site_setting:
-                return redirect('admin:siteSetting')
-
+            
+            if request.user.is_authenticated:
+                if request.user.is_superuser:
+                    redirect_route = self.SiteSettingCheck(request)
+            else:
+                return redirect('admin:login')
+            
+            if redirect_route is not None:
+                return redirect(redirect_route)
+            
         return self.get_response(request)
+
+    def SiteSettingCheck(self, request):
+        # Check if SiteSetting has the first row
+        try:
+            site_setting = SiteSetting.objects.first()
+        except ObjectDoesNotExist:
+            site_setting = None
+
+        if not site_setting:
+            return redirect('admin:siteSetting')
+
+        return None
